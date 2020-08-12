f11987baa0f269e663ce6f13b6131dd64696be5d
Kyle He <panyanhe99@gmail.com>
2020-08-11 20:05:24 -0700

    bug fix

--- main.py
+++ main.py
@@ -64,6 +64,8 @@ You must use your **REAL NAME** in order to participate. There are no exceptions
 Violation of the above rules may result in a mute and/or, in severe cases, revoking of your Cyber Chip. ''')
     server = bot.get_guild(728821753182289961)
     message = await bot.wait_for('message', timeout=1000, check=lambda m: m.author.id == member.id)
+    if (m.author.id == 729370299304247337):
+        return
     guildmember = server.get_member(message.author.id)
 
     await guildmember.edit(nick=message.content)
